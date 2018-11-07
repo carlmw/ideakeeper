0366d020871743634de9452524ea869f27f8c8bd
Carl Whittaker <carl359@gmail.com>
2018-11-07 18:11:04 +0000

    Handle the async side effects of receiving ideas from the API

---
+++ src/sagas/ideas.js
@@ -0,0 +1,31 @@
+import { put, call, takeLatest, takeEvery, all } from 'redux-saga/effects';
+import { createIdea, updateIdea, getAllIdeas } from '../api';
+
+function* addIdea() {
+  const payload = yield call(createIdea);
+
+  yield put({ type: 'NEW_IDEA_RECEIVED', payload });
+}
+
+function* setBody({ payload: { id, body } }) {
+  yield call(updateIdea, id, { body });
+}
+
+function* setTitle({ payload: { id, title } }) {
+  yield call(updateIdea, id, { title });
+}
+
+function* initIdeas() {
+ const payload = yield call(getAllIdeas);
+
+ yield put({ type: 'IDEAS_RECEIVED', payload });
+}
+
+export default function* ideasSaga() {
+  yield all([
+    initIdeas(),
+    takeLatest('ADD_IDEA', addIdea),
+    takeEvery('SET_BODY', setBody),
+    takeEvery('SET_TITLE', setTitle)
+  ]);
+};
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,1 +1,8 @@
-export default function* rootSaga() {};
+import { all } from 'redux-saga/effects';
+import ideas from './ideas';
+
+export default function* rootSaga() {
+  yield all([
+    ideas(),
+  ]);
+};
